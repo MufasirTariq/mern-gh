8cedef48dd5f4450c98c2223c0fdc38b6641c5f7
Mufasir Tariq <mufasirtariq@gmail.com>
2024-10-09 16:35:57 +0500

    setup signup component in react

--- frontend/src/Components/SignIn.js
+++ frontend/src/Components/SignIn.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SignIn = () => {
   return (
-    <div>SignIn</div>
+    <div className='signin'>
+      <h1>SignIn Page</h1>
+
+      <div className='signup-form'>
+      
+        <label>Email:</label>
+        <input type='email' name='email' placeholder='Enter Email' /> <br/>
+        
+        <label>Password:</label>
+        <input type='text' name='password' placeholder='Enter Password' /> <br/>
+
+        <input type='submit' name='signin-btn' value='SignIn' />
+      </div>
+
+      <div>
+        <Link to='/signup'>Don't have an account?</Link>
+        <br/>
+        <Link to='/'>Home Page</Link>
+      </div>
+
+    </div>
   )
 }
 
--- frontend/src/Components/SignUp.js
+++ frontend/src/Components/SignUp.js
@@ -1,8 +1,50 @@
-import React from 'react'
+import {React, useState} from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const SignUp = () => {
+  
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const sendData = async () => {
+    console.log(name, email, password);
+    try {
+          const response = await axios.post('http://localhost:5000/api/user/signup',{name, email, password});
+          console.log("Success",response);
+    } catch (error) {
+      console.log("Error",error);
+    }
+
+  }
+
+
   return (
-    <div>SignUp</div>
+    <div className='signup'>
+      <h1>Signup Page</h1>
+
+      <div className='signup-form'>
+        
+        <label>Name:</label>
+        <input type='text' name='name' placeholder='Enter Name' onChange={(e) => {setName(e.target.value)}} /> <br/>
+        
+        <label>Email:</label>
+        <input type='email' name='email' placeholder='Enter Email' onChange={(e) => {setEmail(e.target.value)}} /> <br/>
+        
+        <label>Password:</label>
+        <input type='text' name='password' placeholder='Enter Password' onChange={(e) => {setPassword(e.target.value)}} /> <br/>
+
+        <input type='submit' name='signup-btn' value='SignUp' onClick={() => sendData()} />
+      </div>
+
+      <div>
+        <Link to='/signin'>Already have an account?</Link>
+        <br/>
+        <Link to='/'>Home Page</Link>
+      </div>
+
+    </div>
   )
 }
 
