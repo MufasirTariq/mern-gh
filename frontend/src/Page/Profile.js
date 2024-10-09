52d99c47d083ac976cb4d6b7f471f4c86bc7b529
Mufasir Tariq <mufasirtariq@gmail.com>
2024-10-09 17:18:41 +0500

    setup logout and token clear

--- frontend/src/Page/Profile.js
+++ frontend/src/Page/Profile.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 const Profile = () => {
 
   const user = JSON.parse(localStorage.getItem('User'));
-
+  const navigate = useNavigate();
   return (
     <div className='profile'>
       <h1>Home Page</h1>
@@ -17,8 +17,12 @@ const Profile = () => {
       )}
       
       <Link to='/'>Home</Link>
-      <br/>
-      <Link to='/logout'>Logout</Link>
+      <button type='submit' name='logout-btn' 
+      onClick={() => {
+        localStorage.clear();
+        navigate('/')
+        }}>Logout</button>
+      
     </div>
   )
 }
